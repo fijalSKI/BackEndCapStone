b3b0aab252a8ae038cf1a5c07cc4ac78434b090e
Alan Fijal <alan.fijal@gmail.com>
2024-07-10 12:16:05 +0200

    fourth

--- backend_capstone/settings.py
+++ backend_capstone/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'HOST' : '127.0.0.1',
         'PORT' : '3306',
         'USER' : 'root',
-        'PASSWORD' : 'Alan2004!',
+        'PASSWORD' : 'root@123',
         #root@123
     }
 }
